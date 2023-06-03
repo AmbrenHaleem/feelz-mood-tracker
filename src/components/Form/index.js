5d69b1e98c292f4d98997bf3fb9a8f0e1f80f490
AmbrenHaleem <118473884+AmbrenHaleem@users.noreply.github.com>
2023-06-02 23:35:54 -0400

    [#5] Implement the activity list update after adding new activity type.
    Closes #5

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,12 +1,12 @@
-import { useState,useEffect } from 'react';
-import { View, Text, TextInput, Keyboard,Pressable, ActivityIndicator, ScrollView } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Text, TextInput, Keyboard, Pressable, ActivityIndicator, ScrollView } from 'react-native';
 import styles from './styles';
 import { primaryColor } from '../../includes/variable';
 import { SelectList } from 'react-native-dropdown-select-list';
 import { useNavigation } from '@react-navigation/native';
-import * as localDB from '../../database/localdb'; 
+import * as localDB from '../../database/localdb';
 
-export default function Form(){
+export default function Form() {
     const navigation = useNavigation();
     const [selectedMood, setSelectedMood] = useState('');
     const [selectedActivity, setSelectedActivity] = useState('');
@@ -19,15 +19,15 @@ export default function Form(){
         navigation.navigate('AddMoodType')
     }
     const handleAddMoodButtonPress = () => {
-       console.log('Mood Saved');
+        console.log('Mood Saved');
     }
     const moodData = [
-        {key:'1', value:'Sad'},
-        {key:'2', value:'Happy'},
-        {key:'3', value:'Angry'},
+        { key: '1', value: 'Sad' },
+        { key: '2', value: 'Happy' },
+        { key: '3', value: 'Angry' },
     ]
 
-     //const activityData = [];
+    //const activityData = [];
     //     {key:'1', value:'Play'},
     //     {key:'2', value:'Read'},
     //     {key:'3', value:'Sleep'},
@@ -49,7 +49,7 @@ export default function Form(){
                         key: item.id,
                         value: item.activity
                     }
-                  });
+                });
                 setActivities(activityData);
                 //console.log('Added Data:',activityData);
             }
@@ -61,51 +61,51 @@ export default function Form(){
 
     return (
         <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.dateTimeLabel}>{new Date().toLocaleString()}</Text>
-          
-          <View style={styles.selectlist.container}>
-            <View style={styles.selectlist.list} >
-                <SelectList placeholder='Select Mood'
-                        setSelected={(val) => setSelectedMood(val)} 
-                        data={moodData} 
-                        save="value"
+            <View style={styles.container}>
+                <Text style={styles.dateTimeLabel}>{new Date().toLocaleString()}</Text>
+
+                <View style={styles.selectlist.container}>
+                    <View style={styles.selectlist.list} >
+                        <SelectList placeholder='Select Mood'
+                            setSelected={(val) => setSelectedMood(val)}
+                            data={moodData}
+                            save="value"
+                        />
+                    </View>
+                    <Pressable style={styles.selectlist.button} onPress={handleAddMoodTypeButtonPress}>
+                        <Text style={styles.button.text}>+</Text>
+                    </Pressable>
+                </View>
+
+                <View style={styles.selectlist.container}>
+                    <View style={styles.selectlist.list} >
+                        <SelectList placeholder='Select Activity'
+                            setSelected={(val) => setSelectedActivity(val)}
+                            data={activities}
+                            save="value"
+                        />
+                    </View>
+                    <Pressable style={styles.selectlist.button} onPress={handleAddActivityButtonPress}>
+                        <Text style={styles.button.text}>+</Text>
+                    </Pressable>
+                </View>
+
+                <Text style={styles.label}>Description:</Text>
+                <TextInput
+                    maxLength={150}
+                    style={styles.multilineTextbox}
+                    multiline numberOfLines={4}
+                    placeholder='Enter Contact Details'
                 />
+                <Pressable style={styles.button.container} onPress={handleAddMoodButtonPress}>
+                    <Text style={styles.button.text}>Add Mood</Text>
+                </Pressable>
             </View>
-            <Pressable style={styles.selectlist.button} onPress={handleAddMoodTypeButtonPress}>
-                <Text style={styles.button.text}>+</Text>
-            </Pressable>
-          </View>
 
-          <View style={styles.selectlist.container}>
-            <View style={styles.selectlist.list} >
-                <SelectList placeholder='Select Activity'
-                    setSelected={(val) => setSelectedActivity(val)} 
-                    data={activities} 
-                    save="value"
-                />
-            </View>
-            <Pressable style={styles.selectlist.button} onPress={handleAddActivityButtonPress}>
-                <Text style={styles.button.text}>+</Text>
-            </Pressable>
-        </View>
 
-          <Text style={styles.label}>Description:</Text>
-          <TextInput
-            maxLength={150}
-            style={ styles.multilineTextbox}
-            multiline numberOfLines={4}
-            placeholder='Enter Contact Details'
-          />
-          <Pressable style={styles.button.container} onPress={handleAddMoodButtonPress}>
-            <Text style={styles.button.text}>Add Mood</Text>
-          </Pressable>
-        </View>
-
-        
         </ScrollView>
 
-        
-      );
-    
+
+    );
+
 }
--- src/components/screens/MoodsScreen/AddMoodType/index.js
+++ src/components/screens/MoodsScreen/AddMoodType/index.js
@@ -1,89 +1,89 @@
-import { Text,View, TextInput,Pressable, ScrollView,ActivityIndicator } from 'react-native';
+import { Text, View, TextInput, Pressable, ScrollView, ActivityIndicator } from 'react-native';
 import { useState } from 'react';
 import styles from './styles';
 import { primaryColor } from '../../../../includes/variable';
-export default function AddMoodType({navigation}){
-    const [moodType, setMoodType] = useState('');
-    const [errorMessages, setErrorMessages] = useState([]);
-    const [savingDate, setSavingData] = useState(false);
+export default function AddMoodType({ navigation }) {
+  const [moodType, setMoodType] = useState('');
+  const [errorMessages, setErrorMessages] = useState([]);
+  const [savingDate, setSavingData] = useState(false);
 
-    const handleMoodTypeChange = (value) => {
-        setMoodType(value);
+  const handleMoodTypeChange = (value) => {
+    setMoodType(value);
+  }
+
+  const handleAddPress = async () => {
+    try {
+      const validate = [];
+
+      // validate the data
+      if (moodType === '') {
+        validate.push('Mood Type is required.');
       }
+      if (validate.length > 0) {
+        setErrorMessages(validate);
+      }
+      else {
 
-      const handleAddPress = async () => {
-        try{
-            const validate = [];
-    
-            // validate the data
-            if (moodType === ''){
-                validate.push('Mood Type is required.');
-            }
-            if (validate.length > 0) {
-                setErrorMessages(validate);
-            }
-            else {
-             
-                const data = {
-                  moodType : moodType,
-                }
-                //console.log("Add data : ", data);
-                setSavingData(true);
-                const id = await database.save(data);
-                setSavingData(false);
-    
-                if (id){
-                  //  props.onAddItem(id, itemDescription, itemQuantity,itemCost,purchaseDate,expiryDate);
-                   
-                    setMoodType('');
-                    setErrorMessages([]);
-                    Keyboard.dismiss();
-                   // props.navigation.goBack();
-                }
-            }
+        const data = {
+          moodType: moodType,
         }
-        catch(err){
-            console.error(err)
+        //console.log("Add data : ", data);
+        setSavingData(true);
+        const id = await database.save(data);
+        setSavingData(false);
+
+        if (id) {
+          //  props.onAddItem(id, itemDescription, itemQuantity,itemCost,purchaseDate,expiryDate);
+
+          setMoodType('');
+          setErrorMessages([]);
+          Keyboard.dismiss();
+          // props.navigation.goBack();
         }
+      }
     }
+    catch (err) {
+      console.error(err)
+    }
+  }
 
-    if (savingDate){
-        return (
-            <View style={styles.loadingContainer}>
-                <ActivityIndicator size = 'large' color = {primaryColor}/>
-                <Text style={styles.loadingText}> Saving data!</Text>
-                <Text style={styles.loadingText}> Please, wait...</Text>
-            </View>
-        );
-      }
-return (
+  if (savingDate) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size='large' color={primaryColor} />
+        <Text style={styles.loadingText}> Saving data!</Text>
+        <Text style={styles.loadingText}> Please, wait...</Text>
+      </View>
+    );
+  }
+  return (
     <ScrollView>
-    <View style={styles.container}>
+      <View style={styles.container}>
 
-      {errorMessages.length > 0 && (
-        <View style={styles.errorMessage.container}>
-        <Text style={styles.errorMessage.label}> Invalid Data:</Text>
-        {errorMessages.map((message,index) => (
-             <Text key={index} style={styles.errorMessage.text}>
-             - {message}
-             </Text>
-        )
+        {errorMessages.length > 0 && (
+          <View style={styles.errorMessage.container}>
+            <Text style={styles.errorMessage.label}> Invalid Data:</Text>
+            {errorMessages.map((message, index) => (
+              <Text key={index} style={styles.errorMessage.text}>
+                - {message}
+              </Text>
+            )
+            )}
+          </View>
         )}
+        <View style={styles.container}>
+          <Text style={styles.label}>Mood Type:</Text>
+          <TextInput
+            maxLength={150}
+            onChangeText={handleMoodTypeChange}
+            defaultValue={moodType}
+            style={styles.textbox}
+          />
+          <Pressable style={styles.button.container} onPress={handleAddPress} >
+            <Text style={styles.button.text}>Add</Text>
+          </Pressable>
         </View>
-      )}
-    <View style={styles.container}>
-            <Text style={styles.label}>Mood Type:</Text>
-      <TextInput
-        maxLength={150}
-        onChangeText={handleMoodTypeChange}
-        defaultValue={moodType}
-        style={styles.textbox}
-      />
-       <Pressable style={styles.button.container} onPress={handleAddPress} >
-        <Text style={styles.button.text}>Add</Text>
-      </Pressable>
-    </View>
-    </View>
+      </View>
     </ScrollView>
-    )
+  )
 }
