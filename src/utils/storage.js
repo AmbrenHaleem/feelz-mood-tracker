7f8889899b6199a97f42cb4558742df325801647
Junying Liu <116390595+junyingliuaaa@users.noreply.github.com>
2023-06-22 22:01:08 -0400

    fix problem about number 11

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -23,3 +23,26 @@ export const getNotis = async () => {
     // error reading value
   }
 }
+
+export const addTime = async (noti) => {
+  try {
+    const jsonValue = JSON.stringify(noti)
+    await AsyncStorage.setItem('@time', jsonValue)
+  } catch (e) {
+    // saving error
+  }
+}
+
+
+export const getTime = async () => {
+  try {
+    const value = await AsyncStorage.getItem('@time')
+    if (value !== null) {
+      return JSON.parse(value);
+    } else {
+      return [];
+    }
+  } catch (e) {
+    // error reading value
+  }
+}
